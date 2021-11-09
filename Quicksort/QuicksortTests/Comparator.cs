dfac0a53dea812f18062a6b9aa2c6243cdcc8d05
Dan <daniladenisov729@gmail.com>
2021-11-09 21:40:37 +0300

    Quicksort: add Comparator, add integer tests

---
+++ Quicksort/QuicksortTests/Comparator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuicksortTests
+{
+    public class Comparator<T> : IComparer<T>
+    {
+        private readonly Comparison<T> _cmp;
+        private int _count = 0;
+        public int Count => _count;
+
+        public Comparator(Comparison<T> cmp)
+        {
+            _cmp = cmp;
+        }
+        public int Compare(T x, T y)
+        {
+            _count++;
+            return _cmp(x, y);
+        }
+
+        public void Reset()
+        {
+            _count = 0;
+        }
+    }
+}
--- Quicksort/QuicksortTests/QuicksortTests.cs
+++ Quicksort/QuicksortTests/QuicksortTests.cs
@@ -9,12 +9,22 @@ namespace QuicksortTests
         [Fact]
         public void Test()
         {
-            int[] arr1, arr2;
-            arr1 = new int[] { 3, 7, 2, 6, 4, 8, 1, 5 };
-            arr2 = (int[])arr1.Clone();
-            
-            Array.Sort(arr1);
-            Quicksort.Sort(arr2);
+            // Integer
+            var cmpInt = new Comparator<int>((x, y) => x - y);
+            TestSample(new int[] { 3, 7, 2, 6, 4, 8, 1, 5 }, cmpInt);
+            TestSample(new int[] { 1, 1, 1, 1, 1 }, cmpInt);
+            TestSample(new int[] { 2, 1, 3, 1, 2 }, cmpInt);
+            TestSample(new int[] { -1, 0, -6, 8, 4, 15, -3, 7, 7, 7, -6 }, cmpInt);
+        }
+
+        public void TestSample<T>(T[] arr, Comparator<T> cmp)
+        {
+            T[] arr1, arr2;
+            arr1 = (T[])arr.Clone();
+            arr2 = (T[])arr.Clone();
+
+            Array.Sort(arr1, cmp);
+            Quicksort.Sort(arr2, cmp);
             Assert.Equal(arr1, arr2);
         }
     }
