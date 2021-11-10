ff95b804391aa21c796e2b2056e0abe35b587289
Dan <daniladenisov729@gmail.com>
2021-11-10 11:42:16 +0300

    Quicksort: add float tests

--- Quicksort/QuicksortTests/QuicksortTests.cs
+++ Quicksort/QuicksortTests/QuicksortTests.cs
@@ -15,6 +15,21 @@ namespace QuicksortTests
             TestSample(new int[] { 1, 1, 1, 1, 1 }, cmpInt);
             TestSample(new int[] { 2, 1, 3, 1, 2 }, cmpInt);
             TestSample(new int[] { -1, 0, -6, 8, 4, 15, -3, 7, 7, 7, -6 }, cmpInt);
+
+            // Float
+            var cmpFloat = new Comparator<float>((x, y) => Math.Sign(x - y));
+            TestSample(new float[] { 0.0345f, 9.8f, 1.23f, 4.567f }, cmpFloat);
+            TestSample(new float[] { 3.92781f, 3.92781f, 3.92781f }, cmpFloat);
+            TestSample(new float[] { 3.92781243729f, 3.92781243729f, 3.92781243729f }, cmpFloat);
+            TestSample(new float[] { 
+                1.234567891234567892f, 
+                1.234567891234567891f, 
+                1.234567891234567896f,
+                1.234567891234567894f,
+                1.234567891234567897f }, cmpFloat);
+            TestSample(new float[] {
+                0, 0.05735f, -34.58263f, 123.16f, -16f,
+                47f, -20.791784f, 58.1623f }, cmpFloat);
         }
 
         public void TestSample<T>(T[] arr, Comparator<T> cmp)
