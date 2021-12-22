06b3cb2516f058b819e2c83bdc07ed720c8d50c2
Dan <daniladenisov729@gmail.com>
2021-12-22 16:56:38 +0300

    Trades: separate table creation, add output to file

--- Trades/Trades/Program.cs
+++ Trades/Trades/Program.cs
@@ -9,19 +9,21 @@ namespace Trades
 {
     class Program
     {
-        private static readonly string _filename = Environment.CurrentDirectory + "/Resources/trades.zip";
+        private static readonly string _inputFile = 
+            Environment.CurrentDirectory + "/Resources/trades.zip";
+        private static readonly string _outputFile = 
+            Environment.CurrentDirectory + "/output/output.txt";
         private static string[] _keys;
 
         static void Main()
         {
-            using var zip = ZipFile.OpenRead(_filename);
+            using var zip = ZipFile.OpenRead(_inputFile);
             using var stream = zip.GetEntry("trades.txt").Open();
             using var reader = new StreamReader(stream);
 
             // Get dict keys
             _keys = Enumerate(reader).First().Split('\t');
 
-            // Get board related entries
             var trades = Enumerate(reader)
                 .Select(x => Split(x))
                 .Where(x => x["SECBOARD"] == "TQBR" || x["SECBOARD"] == "FQBR")
@@ -39,37 +41,16 @@ namespace Trades
             
             var stocks = trades.ToList();
 
-            Console.WriteLine("Luckiest stocks:\n");
-            Console.WriteLine(" SecurityCode | OpeningPrice | ClosingPrice | Increase | Percentage | Volume ");
-            Console.WriteLine("-----------------------------------------------------------------------------");
-            foreach (var stock in stocks.Take(10))
-            {
-                double increase = stock.ClosingPrice - stock.OpeningPrice;
-                Console.WriteLine(
-                    $" {stock.SecurityCode,-12} |" +
-                    $" {stock.OpeningPrice,-12} |" +
-                    $" {stock.ClosingPrice,-12} |" +
-                    $" {increase,-8} |" +
-                    $" {(increase >= 0 ? "+" : "") + $"{increase / stock.OpeningPrice:f5}%",-10} |" +
-                    $" {stock.TotalVolume,-6} ");
-            }
+            string luckiestStocks = StocksTable("LuckiestStocks", stocks.Take(10));
+            string unluckiestStocks = StocksTable("UnluckiestStocks", stocks.TakeLast(10));
 
-            Console.WriteLine();
+            // Output to console
+            Console.WriteLine(luckiestStocks);
+            Console.WriteLine(unluckiestStocks);
 
-            Console.WriteLine("Unluckiest stocks:\n");
-            Console.WriteLine(" SecurityCode | OpeningPrice | ClosingPrice | Increase | Percentage | Volume ");
-            Console.WriteLine("-----------------------------------------------------------------------------");
-            foreach (var stock in stocks.TakeLast(10))
-            {
-                double increase = stock.ClosingPrice - stock.OpeningPrice;
-                Console.WriteLine(
-                    $" {stock.SecurityCode,-12} |" +
-                    $" {stock.OpeningPrice,-12} |" +
-                    $" {stock.ClosingPrice,-12} |" +
-                    $" {increase,-8} |" +
-                    $" {(increase >= 0 ? "+" : "") + $"{increase / stock.OpeningPrice:f5}%",-10} |" +
-                    $" {stock.TotalVolume,-6} ");
-            }
+            // Output to file
+            Directory.CreateDirectory(Path.GetDirectoryName(_outputFile));
+            File.WriteAllText(_outputFile, string.Join("\n", luckiestStocks, unluckiestStocks));
         }
 
         private static IEnumerable<string> Enumerate(StreamReader reader)
@@ -86,6 +67,32 @@ namespace Trades
         {
             return new Dictionary<string, string>(_keys.Zip(line.Split('\t'),
                 (x, y) => KeyValuePair.Create(x, y)));
+        }
+
+        private static string StocksTable(string header, IEnumerable<dynamic> stocks)
+        {
+            int leftPadding = (77 - header.Length) / 2;
+            int rightPadding = 77 - header.Length - leftPadding;
+            string headerCentered = 
+                new string('~', leftPadding) + header + new string('~', rightPadding);
+
+            string table = headerCentered + "\n" +
+                "-----------------------------------------------------------------------------\n" +
+                " SecurityCode | OpeningPrice | ClosingPrice | Increase | Percentage | Volume \n" +
+                "-----------------------------------------------------------------------------\n";
+            foreach (var stock in stocks)
+            {
+                double increase = stock.ClosingPrice - stock.OpeningPrice;
+                table +=
+                    $" {stock.SecurityCode,-12} |" +
+                    $" {stock.OpeningPrice,-12} |" +
+                    $" {stock.ClosingPrice,-12} |" +
+                    $" {increase,-8} |" +
+                    $" {(increase > 0 ? "+" : "") + $"{increase / stock.OpeningPrice:f5}%",-10} |" +
+                    $" {stock.TotalVolume,-6} \n";
+            }
+
+            return table;
         }
     }
 }
